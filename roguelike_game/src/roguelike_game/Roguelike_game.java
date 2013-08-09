6a61c9d763b130605d3c98f332198827460f4561
Andy <andyafw813@gmail.com>
2013-08-09 09:33:59 -0500

    first commit of roguelike class base

---
+++ roguelike_game/src/roguelike_game/Roguelike_game.java
@@ -0,0 +1,123 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roguelike_game;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * @version @author andyafw
+ */
+public class Roguelike_game extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public Painting painting;
+
+    public Roguelike_game() {
+        super("test");
+        painting = new Painting(this);
+        add(painting);
+    }
+
+    public static void main(String[] args) {
+
+        Roguelike_game game = new Roguelike_game();
+        game.setSize(1100, 600);
+        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.setLocationRelativeTo(null);
+        game.setVisible(true);
+        Thread thread = new Thread(game.painting);
+        thread.start();
+    }
+
+    public Color blend(Color clOne, Color clTwo, float fAmount) {
+        float fInverse = (float) (1.0 - fAmount);
+
+        // I had to look up getting colour components in java.  Google is good :)
+        float afOne[] = new float[3];
+        clOne.getColorComponents(afOne);
+        float afTwo[] = new float[3];
+        clTwo.getColorComponents(afTwo);
+
+        float afResult[] = new float[3];
+        afResult[0] = afOne[0] * fAmount + afTwo[0] * fInverse;
+        afResult[1] = afOne[1] * fAmount + afTwo[1] * fInverse;
+        afResult[2] = afOne[2] * fAmount + afTwo[2] * fInverse;
+
+        return new Color(afResult[0], afResult[1], afResult[2]);
+    }
+
+    private class Painting extends JPanel implements Runnable {
+
+        Roguelike_game game;
+        Color color;
+        Color darker;
+        int time = 0;
+
+        public Painting(Roguelike_game game) {
+            this.game = game;
+            color = new Color(0, 255, 0);
+            darker = blend(color, Color.black, 0.25f);
+            this.setPreferredSize(new Dimension(1100, 600));
+        }
+
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+//            System.out.println("getting graphics");
+//            Color shadow = game.blend(originalColour, Color.black, 0.25f);
+//
+//
+//            int width = 1100 / 3;
+//            System.out.println("drawing");
+//            g.setColor(highlight);
+//            g.fillRect(0, 0, width, 600);
+//            g.setColor(originalColour);
+//            g.fillRect(width, 0, width, 600);
+//            g.setColor(shadow);
+//            g.fillRect(width * 2, 0, width, 600);
+
+            int width = 300;
+            int x = 1100 / 2 - width / 2 - 50;
+            int y = 600 / 2 - width / 2;
+
+            g.setColor(Color.black);
+            g.fillRect(0, 0, 1100, 600);
+            g.setColor(Color.green);
+            g.fillArc(x, y, width, width, time * 6, 6);
+            g.setColor(darker);
+            g.drawArc(x, y, width, width, 0, 360);
+            
+            
+            Font f = new Font(Font.MONOSPACED, Font.BOLD, 100);
+            
+            g.setFont(f);
+            g.drawString(Integer.toString(time * -1), x + width / 2 - 25, y + width / 2 + 80);
+            
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    time--;
+                    if (time > 60) {
+                        time = 0;
+                    }
+                    repaint();
+                    System.out.println("time = " + time);
+                    Thread.sleep(1000);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+}
