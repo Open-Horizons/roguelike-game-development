09624e29399df2292e7910096c38c80654d84558
Andy <andyafw813@gmail.com>
2013-08-11 00:16:00 -0500

    made class for racetype. andy out for the night

---
+++ roguelike_game/src/roguelike_game/entity/RaceType.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roguelike_game.entity;
+
+/**
+ *
+ * @author andyafw
+ */
+public class RaceType {
+    public static RaceType ORC        = new RaceType();
+    public static RaceType GOBLIN     = new RaceType();
+    public static RaceType GNOME      = new RaceType();
+    public static RaceType HUMAN      = new RaceType();
+    public static RaceType ELF        = new RaceType();
+    public static RaceType BLOODELF   = new RaceType();
+    public static RaceType CATPERSON  = new RaceType();
+    public static RaceType HALFDRAGON = new RaceType();
+    public static RaceType FISHMAN    = new RaceType();
+    public static RaceType HALFLING   = new RaceType();
+    public static RaceType UNDEAD     = new RaceType();
+    public static RaceType VAMPIRE    = new RaceType();
+    public static RaceType LIZARDMAN  = new RaceType();
+    
+    public RaceType() {
+        
+    }
+}
