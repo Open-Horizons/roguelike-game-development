c8cb4bad8526f759ff8683d6d1277fd35c187c49
Andy <andyafw813@gmail.com>
2013-08-12 12:35:32 -0500

    fixed sprite sheet a little bit and changed sprite class

--- roguelike_game/src/roguelike_game/graphics/Sprite.java
+++ roguelike_game/src/roguelike_game/graphics/Sprite.java
@@ -5,8 +5,8 @@ import java.awt.image.BufferedImage;
 public class Sprite {
 
     public static Sprite PLAYER = new Sprite(64, 0, 1, SpriteSheet.CHARACTERS);
-    public static Sprite EMPTY  = new Sprite(64, 1, 0, SpriteSheet.CHARACTERS);
-    public static Sprite WALL   = new Sprite(64, 2, 0, SpriteSheet.CHARACTERS);
+    public static Sprite EMPTY  = new Sprite(64, 0, 0, SpriteSheet.CHARACTERS);
+    public static Sprite WALL   = new Sprite(64, 1, 0, SpriteSheet.CHARACTERS);
     
     public final int SIZE;
     public int x, y;
