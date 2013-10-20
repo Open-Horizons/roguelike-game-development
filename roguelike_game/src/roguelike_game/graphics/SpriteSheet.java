8d991c7ed1e35b6f011ed3f4a270078665b58629
Sycokinetic <akg7634@gmail.com>
2013-10-20 14:56:34 -0500

    Changed build.xml and SpriteSheet.java to properl jar and reference res/

--- roguelike_game/src/roguelike_game/graphics/SpriteSheet.java
+++ roguelike_game/src/roguelike_game/graphics/SpriteSheet.java
@@ -6,17 +6,18 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class SpriteSheet {
-	public static SpriteSheet CHARACTERS = new SpriteSheet("res/sprites/character.png");
-	public static SpriteSheet SPRITES = new SpriteSheet("res/sprites/sprites.png");
-    public static SpriteSheet WEAPONS = new SpriteSheet("res/sprites/weapons2.png");
-    public static SpriteSheet WALLS = new SpriteSheet("res/sprites/walls.png");
+	public static SpriteSheet CHARACTERS = new SpriteSheet("/res/sprites/character.png");
+	public static SpriteSheet SPRITES = new SpriteSheet("/res/sprites/sprites.png");
+    public static SpriteSheet WEAPONS = new SpriteSheet("/res/sprites/weapons2.png");
+    public static SpriteSheet WALLS = new SpriteSheet("/res/sprites/walls.png");
         
 	private BufferedImage bs;
 	
 	public SpriteSheet(String filename) {		
+		System.out.println(System.getProperty("user.dir"));
 		try {
-			bs = ImageIO.read(new File(filename));
-			//bs = ImageIO.read(SpriteSheet.class.getResource(filename));
+			//bs = ImageIO.read(new File(filename));
+			bs = ImageIO.read(SpriteSheet.class.getResource(filename));
 		} catch (IOException e) {
 			System.out.println("spritesheet can not be read in");
 			e.printStackTrace();
